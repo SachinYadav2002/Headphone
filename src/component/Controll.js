43a487dc26120ddc69af193a98875ebe1bd82561
SachinYadav2002 <yadavsachin7249407392@gmail.com>
2026-01-06 19:11:24 +0530

    Updated Controll component

--- src/component/Controll.js
+++ src/component/Controll.js
@@ -2,7 +2,7 @@ import React from "react";
 import img from "./image/45.webp";
 const Controll = () => {
   return (
-    <div className="md:max-w-[85%] max-w-[95%] flex justify-center py-10 mx-auto p-2 bg-gradient-65 from-yellow-300 to-pink-500 transform h-screen   ">
+    <div className="md:max-w-[85%] max-w-[95%] flex justify-center py-10 mx-auto p-2 bg-gradient-65 from-yellow-300 to-pink-500 transform    ">
       <div className="items-center justify-center sm:flex">
         <div className="p-10 md:w-1/2">
           <h1 className="text-4xl font-bold text-transparent sm:text-6xl bg-gradient-to-tr from-green-600 to-blue-600 bg-clip-text">
@@ -19,7 +19,7 @@ const Controll = () => {
               SOLO
             </h1>
             <img
-              className="absolute left-0 right-0 ml-auto mr-auto w-[400px] xl:w-[600px]"
+              className="absolute left-0 right-0 ml-auto mr-auto w-[300px] xl:w-[600px] "
               src={img}
             ></img>
           </div>
